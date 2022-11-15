9f7bbeebd52f19b9b907f291b9dedce3d1ec75d6
Grazi Felix <grazielafelix2001@gmail.com>
2022-11-14 21:04:13 -0300

    feat: exercicios iniciais

---
+++ 1.EstruturaSequencial/1.ola-mundo.py
@@ -0,0 +1,5 @@
+# Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+# Graziela Maria
+# 14/11/22
+
+print('Olá Mundo')
---
+++ 1.EstruturaSequencial/3.soma.py
@@ -0,0 +1,8 @@
+# Faça um Programa que peça dois números e imprima a soma.
+# Graziela Maria
+# 14/11/22
+
+num1 = int(input("informe o primeiro número: "))
+num2 = int(input("informe o segundo número:"))
+
+print(f"A soma é igual: {num1+num2}")
