a11935d37ff243d113342c57219f9584cd7abe57
Graziela Felix <grazielafelix.dev@gmail.com>
2022-11-16 17:47:35 -0300

    feat: exercícios 5-8

---
+++ 1.EstruturaSequencial/5.metros-para-centimetros.py
@@ -0,0 +1,6 @@
+# Faça um Programa que converta metros para centímetros.
+# Graziela Maria
+# 16/11/22
+
+metros = float(input("Insira o numero de metros: "))
+print(f"{metros}m em centímetros é igual a {metros*100}cm")
---
+++ 1.EstruturaSequencial/6.area-do-circulo.py
@@ -0,0 +1,6 @@
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área
+# Graziela Maria
+# 16/11/22
+
+raio = float(input("Ensira o valor do raio do circulo: "))
+print(f"O valor da area do circulo é {3.14*(raio**2)}")
