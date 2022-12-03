0e68aa2d5a8bb5c728880241c41a61d9bb8fa370
Grazi Felix <grazielafelix2001@gmail.com>
2022-12-03 10:54:34 -0300

    feat: adicionando vizualização do hover e click dos quadrados do sudoku

--- 16.ProjetoFinal/Sudoku/src/main.py
+++ 16.ProjetoFinal/Sudoku/src/main.py
@@ -3,11 +3,15 @@
 # Graziela Maria
 
 # Imports
+import math
 import tkinter
 from tkinter import filedialog
-import pygame
+import pygame as pg
 
 # Selecionando um arquivo que representa um sudoko de dentro sistema do usuário
+
+
+
 def abrir_arquivo_do_sistema():
     tk_screen = tkinter.Tk()
     tk_screen.withdraw() # esconde a janela do tkinter
@@ -27,10 +31,9 @@ def abrindo_arquivo():
                 lista.append(int(j))
 
         sudoku.append(lista)
+    print(sudoku)
     return sudoku
 
-# for i in sudoku:
-#     print(i)
 
 # retorna o sudoku resolvido
 def criar_arquivo_resposta(sudoku):
@@ -39,22 +42,80 @@ def criar_arquivo_resposta(sudoku):
         resposta.write(str(i))
         resposta.write('\n')
 
-SCREEN_SIZE = (800, 700)
-pygame.init()
-window = pygame.display.set_mode(SCREEN_SIZE)
-color = (255, 255, 255)
+
+# cores utilizados
+cinza_escuro = (40, 49, 64, 25)
+azul_selecionado = (119, 146, 191, 75)
+azul_brilhante = (158, 195, 255, 100)
+
+# inicializando tela principal
+pg.init()
+window = pg.display.set_mode((800, 700))
+
+# inicializando fontes
+pg.font.init()
+font = pg.font.SysFont('arial', 50, italic=True)
+
+# variaveis utilizadas
+click_last_status = False
+click_position_x = -1
+click_position_y = -1
+numero = 0
+
+def tabuleiro_hover(window, mouse_position_x, mouse_position_y):
+    quadrado = 50
+    ajuste = 30
+    x = math.ceil((mouse_position_x-ajuste)/quadrado)-1
+    y = math.ceil((mouse_position_y-ajuste)/quadrado)-1
+    pg.draw.rect(window, cinza_escuro, (0, 0, 800, 700))
+    if 0 <= x <= 8 and 0 <= y <= 8:
+        pg.draw.rect(window, azul_brilhante, (ajuste + x * quadrado, ajuste + y * quadrado, quadrado, quadrado))
+        print(ajuste + x * quadrado)
+
+def quadrado_selecionado(window, mouse_position_x, mouse_position_y, click_last_status, click, x, y):
+    quadrado = 50
+    ajuste = 30
+    if click_last_status == True and click == True:
+        x = math.ceil((mouse_position_x - ajuste) / quadrado) - 1
+        y = math.ceil((mouse_position_y - ajuste) / quadrado) - 1
+    if 0 <= x <= 8 and 0 <= y <= 8:
+        pg.draw.rect(window, azul_selecionado, (ajuste + x * quadrado, ajuste + y * quadrado, quadrado, quadrado))
+    return x, y
+
+
 running = True
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             running = False
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_SPACE:
+        elif event.type == pg.KEYUP:
+            if event.key == pg.K_SPACE:
                 sudoku = abrindo_arquivo()
                 for i in sudoku:
                     print(i)
+        elif event.type == pg.KEYDOWN:
+            numero = pg.key.name(event.key)
 
-    window.fill(color)
-    pygame.display.flip()
 
-pygame.quit()
+    #mouse
+    mouse = pg.mouse.get_pos()
+    mouse_position_x = mouse[0]
+    mouse_position_y = mouse[1]
+
+    # declarando variavel do mouse
+    click = pg.mouse.get_pressed()
+    print(click)
+
+    #Game
+    tabuleiro_hover(window, mouse_position_x, mouse_position_y)
+    click_position_x, click_position_y = quadrado_selecionado(window, mouse_position_x, mouse_position_y, click_last_status, click[0], click_position_x, click_position_y)
+
+    # click_last_status
+    if click[0] == True:
+        click_last_status = True
+    else:
+        click_last_status = False
+
+    pg.display.flip()
+
+pg.quit()
