36f16b9acd0a0670596f37158f9da7cbc9e79b95
Grazi Felix <grazielafelix2001@gmail.com>
2022-12-06 17:41:53 -0300

    feat: arquivo com as constantes utilizadas no jogo

---
+++ 16.ProjetoFinal_Sudoku/Sudoku/constants.py
@@ -0,0 +1,13 @@
+
+#configuração do pygame.display
+DISPLAY_NAME = 'Sudoku'
+DISPLAY_SIZE = (800, 600)
+
+# cores usadas no sistema
+CORES = {
+    'background_color': (14, 17, 23, 17),
+    'roxo_escuro': (73, 88, 173, 68),
+    'roxo_claro': (105, 127, 250, 98)
+}
+
+FONT = {'name': 'arial', 'size': 45}
