1e027c59ce9290bc8ece311f5a8c972162eee868
Graziela Felix <grazielafelix.dev@gmail.com>
2023-04-05 15:32:15 -0300

    feat: estruturas de decisao exercicios 1-3

---
+++ 02.EstruturasDeDecisao/01.MaiorNumero.py
@@ -0,0 +1,11 @@
+# Faça um Programa que peça dois números e imprima o maior deles.
+# Graziela Felix
+# 05/04/23
+
+primeiroNumero = int(input("Primeiro numero: "))
+segundoNumero = int(input("Segundo numero: "))
+
+if primeiroNumero > segundoNumero:
+    print(f"O maior numero é o primeiro: ", primeiroNumero)
+else:
+    print(f"O maior numero é o segundo: ", segundoNumero)
