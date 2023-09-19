f3562aa53e970184466809e7a91328623002e471
GrazielaFelix <grazielafelix.dev@gmail.com>
2023-09-19 17:55:57 -0300

    feat: creating de views and logic of a form register

--- 01.REST-API-Development-with-Django/register/views.py
+++ 01.REST-API-Development-with-Django/register/views.py
@@ -1,3 +1,20 @@
+from register.forms import RegisterForm
 from django.shortcuts import render
 
 # Create your views here.
+def register(request): 
+    success = False
+    if request.method == 'GET':
+        form = RegisterForm()
+    else:
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            success = True
+            form.save()
+
+    context = {
+        'form': form,
+        'success': success
+    }
+        
+    return render(request, 'register.html', context)
